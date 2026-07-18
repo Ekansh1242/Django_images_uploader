60f95631d3990da616ef0b9bd50389c3c408a8d5
Ekansh1242 <ekansh1242@gmail.com>
2026-07-18 22:13:33 +0530

    Debug static files build failure with set -e

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -e
+set -x
+
 echo "Installing dependencies..."
 pip install --break-system-packages -r requirements.txt
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
+
+echo "Verifying staticfiles directory..."
+ls -la staticfiles
 
 echo "Applying migrations..."
 python manage.py migrate --noinput
